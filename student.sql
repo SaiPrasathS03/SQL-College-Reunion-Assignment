ee85ed9b16ea3ea87a3dcbbb8aceff0aa22ebcad
SaiPrasathS03 <148991466+SaiPrasathS03@users.noreply.github.com>
2025-04-21 20:39:59 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -7,15 +7,13 @@ gender ENUM('Male', 'Female','Other'),
 contact_info VARCHAR(255)
 );
 select * from students;
-INSERT INTO students (name, gender, contact_info) VALUES ('Arun', 'Male', 'arun@example.com'),
- ('Sri', 'Female', 'sri@example.com'),
- ('Vignesh', 'Male', 'vignesh@example.com'),
- ('Gowri', 'Female', 'gowri@example.com'),
- ('Surya', 'Male', 'surya@example.com'),
- ('Rose', 'Female', 'rose@example.com'),
- ('Madhan', 'Male', 'madhan@example.com'),
- ('Nandhini', 'Female', 'nandhini@example.com'),
- ('Ram', 'Male', 'ram@example.com'),
- ('Priya', 'Female', 'revathi@example.com');
-
-
+INSERT INTO students (name, gender, contact_info) VALUES ('Arun', 'Male', 'arun@zsgs.com'),
+ ('Sri', 'Female', 'sri@zsgs.com'),
+ ('Vignesh', 'Male', 'vignesh@zsgs.com'),
+ ('Gowri', 'Female', 'gowri@zsgs.com'),
+ ('Surya', 'Male', 'surya@zsgs.com'),
+ ('Rose', 'Female', 'rose@zsgs.com'),
+ ('Madhan', 'Male', 'madhan@zsgs.com'),
+ ('Nandhini', 'Female', 'nandhini@zsgs.com'),
+ ('Ram', 'Male', 'ram@zsgs.com'),
+ ('Priya', 'Female', 'revathi@zsgs.com');
