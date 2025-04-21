7402cc4ff79fed111ebf213fdd9f5fb547decc65
SaiPrasathS03 <148991466+SaiPrasathS03@users.noreply.github.com>
2025-04-21 20:39:05 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -16,13 +16,6 @@ INSERT INTO students (name, gender, contact_info) VALUES ('Arun', 'Male', 'arun@
  ('Madhan', 'Male', 'madhan@example.com'),
  ('Nandhini', 'Female', 'nandhini@example.com'),
  ('Ram', 'Male', 'ram@example.com'),
- ('Priya', 'Female', 'revathi@example.com'),
- ('Chandru', 'Male', 'chandru@example.com'),
- ('Vishnu', 'Male', 'vishnu@example.com'),
- ('Anitha', 'Female', 'anitha@example.com'),
- ('Jai', 'Male', 'jai@example.com'),
- ('Dhanush', 'Male', 'dhanush@example.com');
- 
- delete from students where student_id between 11 and 15
+ ('Priya', 'Female', 'revathi@example.com');
 
 
