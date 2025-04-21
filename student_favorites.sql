7669bff8c02b9792903f0be742060936521791a0
SaiPrasathS03 <148991466+SaiPrasathS03@users.noreply.github.com>
2025-04-21 20:42:25 +0530

    Update student_favorites.sql

--- student_favorites.sql
+++ student_favorites.sql
@@ -9,12 +9,12 @@ INSERT INTO student_favorites (student_id, favorite_actor, favorite_actress) VAL
 (1, 'Vijay', 'Rashmika Mandanna'),
 (2, 'Rajinikanth', 'Trisha'),
 (3, 'Dhanush', 'Keerthy Suresh'),
-(4, 'Udhayanidhi Stalin', 'Priya Bhavani Shankar'),
-(5, 'Suriya', 'Samantha'),
-(6, 'Vikram', 'Aishwarya Rajesh'),
-(7, 'Sivakarthikeyan', 'Hansika Motwani'),
-(8, 'Karthi', 'Kajal Aggarwal'),
-(9, 'Jayam Ravi', 'Nayanthara'),
+(4, 'Soori', 'Priya Bhavani Shankar'),
+(5, 'Vijay', 'Samantha'),
+(6, 'Ajith Kumar', 'Aishwarya Rajesh'),
+(7, 'Dhanush', 'Hansika Motwani'),
+(8, 'Rajinikanth', 'Kajal Aggarwal'),
+(9, 'Ajith Kumar', 'Nayanthara'),
 (10, 'Ajith Kumar', 'Manju Warrier');
 select * from student_favorites
 
